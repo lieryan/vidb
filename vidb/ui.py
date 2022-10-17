402f6ee4a414d30ac8cca23fe116f5ea01109e78
Lie Ryan <lieryan.24@proton.me>
2022-10-18 01:43:02 +1100

    Refactor into ui.Vidb class

--- vidb/ui.py
+++ vidb/ui.py
@@ -11,61 +11,6 @@ from ptterm import Terminal
 from pygments.lexers.python import PythonLexer
 
 
-source_kb = KeyBindings()
-breakpoint_kb = KeyBindings()
-stacktrace_kb = KeyBindings()
-variables_kb = KeyBindings()
-kb = KeyBindings()
-
-source_code_buffer = Buffer(
-    document=Document(
-        open("vidb/ui.py").read(),
-    ),
-    read_only=True,
-)
-
-source_window = Window(
-    content=BufferControl(
-        buffer=source_code_buffer,
-        lexer=PygmentsLexer(PythonLexer),
-        key_bindings=source_kb,
-    ),
-    left_margins=[
-        NumberedMargin(),
-    ],
-    cursorline=True,
-)
-
-terminal_window = Terminal(
-    ["ipython"],
-    height=10,
-)
-
-variables_window = Window(
-    content=FormattedTextControl(
-        text="Hello world",
-        focusable=True,
-        key_bindings=variables_kb,
-    ),
-)
-
-stacktrace_window = Window(
-    content=FormattedTextControl(
-        text="Hello world",
-        focusable=True,
-        key_bindings=stacktrace_kb,
-    ),
-)
-
-breakpoint_window = Window(
-    content=FormattedTextControl(
-        text="Hello world",
-        focusable=True,
-        key_bindings=breakpoint_kb,
-    ),
-)
-
-
 border_style = "fg:lightblue bg:darkred bold"
 
 
@@ -104,107 +49,168 @@ def VSeparator():
     )
 
 
-main_container = VSplit([
-    HSplit([
-        # Source code buffer
-        source_window,
-
-        HSeparator(),
-
-        # Shell buffer
-        terminal_window,
-    ]),
-
-    # A vertical line in the middle
-    VSeparator(),
-
-    HSplit(
-        [
-            # Variables
-            TitledWindow(
-                "Variables:",
-                variables_window,
-            ),
-
-            # Stack
-            TitledWindow(
-                "Stack:",
-                stacktrace_window,
-            ),
-
-            # Breakpoint
-            TitledWindow(
-                "Breakpoints:",
-                breakpoint_window,
-            ),
-        ],
-        width=40,
-    ),
-])
-
-root_container = TitledWindow(
-    "ViDB 0.1.0 - ?:help  n:next  s:step into  b:breakpoint  !:python command line",
-    main_container,
-    title_style=border_style,
-)
-
-layout = Layout(root_container)
-
-@kb.add("q")
-def exit_(event):
-    event.app.exit()
-    terminal_window.process.kill()
-
-
-def focus_source_window(event):
-    event.app.layout.focus(source_window)
-
-
-@kb.add("V")
-@source_kb.add("right")
-def focus_variable_window(event):
-    event.app.layout.focus(variables_window)
-
-
-@kb.add("S")
-def focus_stacktrace_window(event):
-    event.app.layout.focus(stacktrace_window)
-
-
-@kb.add("B")
-def focus_breakpoint_window(event):
-    event.app.layout.focus(breakpoint_window)
-
-
-@kb.add("X")
-@kb.add("!")
-def focus_terminal_window(event):
-    event.app.layout.focus(terminal_window)
-
-
-variables_kb.add("left")(focus_source_window)
-variables_kb.add("down")(focus_stacktrace_window)
-variables_kb.add("up")(focus_breakpoint_window)
-
-stacktrace_kb.add("left")(focus_source_window)
-stacktrace_kb.add("up")(focus_variable_window)
-stacktrace_kb.add("down")(focus_breakpoint_window)
-
-breakpoint_kb.add("left")(focus_source_window)
-breakpoint_kb.add("down")(focus_variable_window)
-breakpoint_kb.add("up")(focus_stacktrace_window)
-
-
 class UI:
     _ptk: Application
 
     def __init__(self):
+        self._create_keybinds()
         self._ptk = Application(
-            layout=layout,
-            key_bindings=kb,
+            layout=self._create_layout(),
+            key_bindings=self.kb,
             full_screen=True,
             mouse_support=True,
         )
 
     def run(self):
         self._ptk.run()
+
+    def _create_layout(self):
+        root_container = TitledWindow(
+            "ViDB 0.1.0 - ?:help  n:next  s:step into  b:breakpoint  !:python command line",
+            self._create_main_container(),
+        )
+
+        return Layout(root_container)
+
+    def _create_main_container(self):
+        self.source_window = self._create_source_window()
+        self.terminal_window = self._create_terminal_window()
+        self.variables_window = self._create_variables_window()
+        self.stacktrace_window = self._create_stacktrace_window()
+        self.breakpoint_window = self._create_breakpoint_window()
+        return VSplit([
+            HSplit([
+                # Source code buffer
+                self.source_window,
+
+                HSeparator(),
+
+                # Shell buffer
+                self.terminal_window,
+            ]),
+
+            # A vertical line in the middle
+            VSeparator(),
+
+            HSplit(
+                [
+                    # Variables
+                    TitledWindow(
+                        "Variables:",
+                        self.variables_window,
+                    ),
+
+                    # Stack
+                    TitledWindow(
+                        "Stack:",
+                        self.stacktrace_window,
+                    ),
+
+                    # Breakpoint
+                    TitledWindow(
+                        "Breakpoints:",
+                        self.breakpoint_window,
+                    ),
+                ],
+                width=40,
+            ),
+        ])
+
+    def _create_source_window(self):
+        return Window(
+            content=BufferControl(
+                buffer=self._create_source_code_buffer(),
+                lexer=PygmentsLexer(PythonLexer),
+                key_bindings=self.source_kb,
+            ),
+            left_margins=[
+                NumberedMargin(),
+            ],
+            cursorline=True,
+        )
+
+    def _create_source_code_buffer(self):
+        return Buffer(
+            document=Document(
+                open("vidb/ui.py").read(),
+            ),
+            read_only=True,
+        )
+
+    def _create_terminal_window(self):
+        return Terminal(
+            ["ipython"],
+            height=10,
+        )
+
+    def _create_variables_window(self):
+        return Window(
+            content=FormattedTextControl(
+                text="Hello world",
+                focusable=True,
+                key_bindings=self.variables_kb,
+            ),
+        )
+
+    def _create_stacktrace_window(self):
+        return Window(
+            content=FormattedTextControl(
+                text="Hello world",
+                focusable=True,
+                key_bindings=self.stacktrace_kb,
+            ),
+        )
+
+    def _create_breakpoint_window(self):
+        return Window(
+            content=FormattedTextControl(
+                text="Hello world",
+                focusable=True,
+                key_bindings=self.breakpoint_kb,
+            ),
+        )
+
+    def _create_keybinds(self):
+        self.kb = kb = KeyBindings()
+        self.source_kb = source_kb = KeyBindings()
+        self.breakpoint_kb = breakpoint_kb = KeyBindings()
+        self.stacktrace_kb = stacktrace_kb = KeyBindings()
+        self.variables_kb = variables_kb = KeyBindings()
+
+        @kb.add("q")
+        def exit_(event):
+            event.app.exit()
+            self.terminal_window.process.kill()
+
+        def focus_source_window(event):
+            event.app.layout.focus(self.source_window)
+
+        @kb.add("V")
+        @source_kb.add("right")
+        def focus_variable_window(event):
+            event.app.layout.focus(self.variables_window)
+
+        @kb.add("S")
+        def focus_stacktrace_window(event):
+            event.app.layout.focus(self.stacktrace_window)
+
+        @kb.add("B")
+        def focus_breakpoint_window(event):
+            event.app.layout.focus(self.breakpoint_window)
+
+        @kb.add("X")
+        @kb.add("!")
+        def focus_terminal_window(event):
+            event.app.layout.focus(self.terminal_window)
+
+        variables_kb.add("left")(focus_source_window)
+        variables_kb.add("down")(focus_stacktrace_window)
+        variables_kb.add("up")(focus_breakpoint_window)
+
+        stacktrace_kb.add("left")(focus_source_window)
+        stacktrace_kb.add("up")(focus_variable_window)
+        stacktrace_kb.add("down")(focus_breakpoint_window)
+
+        breakpoint_kb.add("left")(focus_source_window)
+        breakpoint_kb.add("down")(focus_variable_window)
+        breakpoint_kb.add("up")(focus_stacktrace_window)
